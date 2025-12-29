bd8a0d3bb042931c127a6651a902ff8ba839a183
Pradyumn Chamoli <pradyumnchamoli18@gmail.com>
2025-12-29 15:02:34 +0530

    Added validation for Reviews

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate=require("ejs-mate"); 
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
-
+const {listingSchema , reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -36,6 +36,16 @@ app.get("/",(req,res)=>{
 
 const validateListing = (req,res,next)=>{
     let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg  = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(404 , errMsg);
+    }else{
+        next();
+    }
+
+}
+const validateReview = (req,res,next)=>{
+    let {error} = reviewSchema.validate(req.body);
     if(error){
         let errMsg  = error.details.map((el)=>el.message).join(",");
         throw new ExpressError(404 , errMsg);
@@ -96,6 +106,26 @@ app.delete("/listings/:id" , wrapAsync(async(req,res)=>{
     console.log(deleteListing);
     res.redirect("/listings");
 }))
+
+
+//Reviews
+//Post Route
+
+app.post("/listings/:id/reviews" ,validateReview  , wrapAsync(async(req,res)=>{
+     let listing = await Listing.findById(req.params.id);
+     let newReview = new Review(req.body.review);
+
+     listing.reviews.push(newReview);
+
+     await newReview.save();
+     await listing.save();
+
+     res.redirect(`/listings/${listing._id}`);
+
+
+}));
+
+
 
 // app.get("/testlisting" , async(req,res)=>{
 //     let samplelisting = new Listing({
